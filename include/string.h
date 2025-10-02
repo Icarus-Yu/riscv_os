9af8dcb1c05638fd0e1837ae8bbd9bca0b3aed7a
Icarus-Yu <hualuck7@gmail.com>
2025-10-02 20:26:21 +0800

    bug小修

--- include/string.h
+++ include/string.h
@@ -1,7 +1,6 @@
-// include/memory.h
-#ifndef __MEMORY_H__
-#define __MEMORY_H__
-
+// include/string.h  <-- (修正注释)
+#ifndef __STRING_H__  // <-- 修改为独一无二的宏
+#define __STRING_H__  // <-- 保持一致
 #include "riscv.h" // 需要 riscv.h 中的类型定义
 
 // --- kalloc.c ---
@@ -13,6 +12,6 @@ void kfree(void *pa);
 // --- vm.c ---
 void kvminit(void);
 void kvminithart(void);
-void *memset(void *dst, int c, unsigned long n);
+void* memset(void *dst, int c, uint64_t n);
 
 #endif // __MEMORY_H__
