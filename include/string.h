e8a1662f82c53c6e41ae67804f2e641eade99464
Icarus-Yu <hualuck7@gmail.com>
2025-10-27 14:55:49 +0800

    收尾

--- include/string.h
+++ include/string.h
@@ -1,6 +1,6 @@
 // include/string.h  <-- (修正注释)
-#ifndef __STRING_H__  // <-- 修改为独一无二的宏
-#define __STRING_H__  // <-- 保持一致
+#ifndef __STRING_H__  
+#define __STRING_H__  
 #include "riscv.h" // 需要 riscv.h 中的类型定义
 
 // --- kalloc.c ---
