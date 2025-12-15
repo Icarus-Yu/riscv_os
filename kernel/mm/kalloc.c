7b000d01f76f9528d9283323e6bde5107d76c0a1
Icarus-Yu <hualuck7@gmail.com>
2025-12-15 16:35:45 +0800

    ex测试完成

--- kernel/mm/kalloc.c
+++ kernel/mm/kalloc.c
@@ -4,8 +4,8 @@
 #include <memory.h>
 #include "string.h"
 
+extern char end[];
 extern char etext[];
-
 // 物理内存结束地址
 #define PHYSTOP 0x88000000L
 
@@ -25,15 +25,15 @@ void kfree(void *pa) {
 }
 
 void kinit() {
-    // 从 etext（现在是 0x80200000 之后）到 PHYSTOP
-    for (char *p = (char*)PGROUNDUP((uint64_t)etext); 
+    // 修改 2: 从 end 开始初始化空闲内存，而不是 etext
+    // end 之后才是真正的空闲物理内存
+    for (char *p = (char*)PGROUNDUP((uint64_t)end); 
          p + PGSIZE <= (char*)PHYSTOP; 
          p += PGSIZE) {
         kfree(p);
     }
     printf("kinit: Physical memory allocator initialized.\n");
 }
-
 void* kalloc(void) {
     struct run *r;
     r = kmem.freelist;
